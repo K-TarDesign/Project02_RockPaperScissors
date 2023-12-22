bd0ee0bf7fc98c96c62f2eb6fc598d744e3f8ab1
Kari T <131820486+K-TarDesign@users.noreply.github.com>
2023-12-22 10:00:27 -1000

    Create JS variables

---
+++ main.js
@@ -0,0 +1,22 @@
+//Variables
+
+    //SCOREBOARD
+    const scoreComp = document.querySelector('#score-computer');
+    const scorePlayer = document.querySelector('#score-player');
+
+    //RESULTS
+    const resultComp = document.querySelector('#computer-result');
+    const resultPlayer = document.querySelector('#player-result');
+
+    //BUTTONS
+    const btnPlay = document.querySelector('#btn-play');
+    const btnRock = document.querySelector('#btn-rock');
+    const btnPaper = document.querySelector('#btn-paper');
+    const btnScissors = document.querySelector('#btn-scissors');
+
+    //DISPLAY BLOCKS
+    const blockPlay = document.querySelector('.play-game');
+    const blockOption = document.querySelector('.option');
+    const blockResults = document.querySelector('.round-results');
+
+
